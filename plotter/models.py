a65a86f486104e19e0cf93deb27711708ccae0ad
luabeltranga <luabeltranga@unal.edu.co>
2022-04-07 22:26:20 -0500

    create db for metadata_countries

--- plotter/models.py
+++ plotter/models.py
@@ -85,3 +85,26 @@ class Data(BaseMeta):
         verbose_name_plural = 'datas'
 
 
+from django.db import models
+
+
+class BaseMeta(models.Model):
+    country_code = models.CharField(max_length=3, verbose_name='Country Code', null=True, blank=True,
+                                    db_column='Country Code')
+    region = models.CharField(max_length=150, verbose_name='Region', null=True, blank=True, db_column='Region')
+    income = models.CharField(max_length=150, verbose_name='Income Group', null=True, blank=True,
+                              db_column='Income Group')
+    special_notes = models.CharField(max_length=150, verbose_name='Special Notes', null=True, blank=True,
+                                     db_column='Special Notes')
+    table_name = models.CharField(max_length=150, verbose_name='Table Name', null=True, blank=True,
+                                  db_column='Table Name')
+
+    class Meta:
+        abstract = True
+
+
+class Metadata(BaseMeta):
+    class Meta:
+        db_table = "Metadata-Countries"
+        verbose_name = 'country'
+        verbose_name_plural = 'countries'
